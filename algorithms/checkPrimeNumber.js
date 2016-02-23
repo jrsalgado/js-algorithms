e5f604d8d11292f55cb9fb874ddabc1094bd660a
Jayro Salgado <jsalgado@nearsoft.com>
2016-02-23 09:14:59 -0700

    a better solution to check if a number is prime

--- algorithms/checkPrimeNumber.js
+++ algorithms/checkPrimeNumber.js
@@ -1,4 +1,5 @@
 module.exports.isPrime = isPrime;
+module.exports.isPrime2 = isPrime2;
 
 function isPrime(n){
   var divisor = 2;
@@ -7,5 +8,19 @@ function isPrime(n){
     if( n%divisor === 0 ){ return false; }
     divisor ++;
   }
+  return true;
+}
+
+// A number will be never divisible by a number up to his root square
+// and any number divisible on a even number will be divisible by 2
+function isPrime2(n){
+  var top = Math.sqrt(n),
+  divisor = 2;
+  
+  while(top > divisor){
+    if( n%divisor === 0 ){ return false; }
+    divisor = (divisor === 2)? 3 : divisor += 2;
+  }
+  
   return true;
 }
--- test/checkPrimeNumber.js
+++ test/checkPrimeNumber.js
@@ -3,12 +3,21 @@
 var chai = require('chai');
 var should = chai.should();
 var isPrime = require('../algorithms/checkPrimeNumber').isPrime;
+var isPrime2 = require('../algorithms/checkPrimeNumber').isPrime2;
 
 describe('check Prime number', function () {
   it('should return true', function () {
     return isPrime(137).should.be.true;
   });
   it('should return false', function () {
-    return isPrime(22).should.be.false;
+    return isPrime(237).should.be.false;
+  });
+});
+describe('better solution check Prime number', function () {
+  it('should return true', function () {
+    return isPrime2(137).should.be.true;
+  });
+  it('should return false', function () {
+    return isPrime2(237).should.be.false;
   });
 });
